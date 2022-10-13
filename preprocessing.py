2b73b487a71f36d39395548c13ff1a62f0d6ee2e
JoshVStaden <joshvstaden14@gmail.com>
2022-10-13 14:29:01 +0200

    Added unit vector functionality

--- preprocessing.py
+++ preprocessing.py
@@ -1,4 +1,4 @@
-from turtle import right
+import numpy as np
 import mediapipe as mp
 mp_pose = mp.solutions.pose
 
@@ -14,3 +14,19 @@ def global_coordinate_frame(kp_arr):
     
     return kp_arr
 
+def dist(point1, point2):
+    term1 = (point1.x + point2.x) ** 2
+    term2 = (point1.y + point2.y) ** 2
+    return np.sqrt(term1 + term2)
+
+def keypoints_to_unit_vectors(kp_arr, pc_arr):
+    unit_vectors = []
+    for pc in pc_arr:
+        point1 = kp_arr[pc[0]]
+        point2 = kp_arr[pc[1]]
+        dist = dist(point1, point2)
+        unit_vectors.append([
+            (point2.x - point1.x) / dist,
+            (point2.y - point1.y) / dist
+        ])
+    return unit_vectors
