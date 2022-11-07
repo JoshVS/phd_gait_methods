b37671d8f3a428d02d9ed7623f6cb572ae8b554f
JoshVStaden <g14v2805@campus.ru.ac.za>
2022-11-07 11:27:16 +0200

    Refactored and added peak thresholding

--- train.py
+++ train.py
@@ -12,52 +12,35 @@ pc = mp_pose.POSE_CONNECTIONS
 
 skel_da = VideoDataset(max_samples=20)
 
-def show_gait_cycle(vid_seq):
-    distances = []
-    for i, kp in enumerate(vid_seq):
-        if (kp == 0):
-            continue
-        # if i == 80:
-        # print(kp)
-    # quit()
-        plt.figure()
-        for p in pc:
-            p1, p2 = p
-            x = [kp[p1][0],kp[p2][0]]
-            y = [kp[p1][1],kp[p2][1]]
-            plt.plot(x,y )
-        # plt.scatter(kp[:][0], kp[:][1])
+def filter_peaks(peaks, threshold=5):
+    prev_peak = 0
+    new_peaks = []
+    for p in peaks:
+        print(p)
+        if p - prev_peak > threshold:
+            new_peaks.append(p)
+        prev_peak = p
+    return new_peaks
 
-        
+def get_peaks(norm_skel_vid):
+    distances = []
+    for i, kp in enumerate(norm_skel_vid):
         ankle1 = kp[mp_pose.PoseLandmark.LEFT_ANKLE]
         ankle2 = kp[mp_pose.PoseLandmark.RIGHT_ANKLE]
-        x = [ankle1[0],ankle2[0]]
-        y = [ankle1[1],ankle2[1]]
-        plt.plot(x,y)
-            
-
-        plt.savefig(f"walk_eg/{i}.png")
-        plt.close()
-        
         distances.append(dist(ankle1, ankle2))
-    plt.figure()
-    # print(distances)
-    # quit()
-    # plt.plot(distances)
-    # plt.plot(find_peaks(distances))
-    # print(find_peaks(distances))
-    # quit()
-    distances = savgol_filter(distances, 9, 3)
+    filtered_distances = savgol_filter(distances, 9, 3)
+    peaks = find_peaks(filtered_distances)[0]
+    peaks = filter_peaks(peaks)
+    return peaks, [filtered_distances[x] for x in peaks], filtered_distances
+
+
+def show_gait_cycle(vid_seq):
+    peaks, peak_vals, distances = get_peaks(vid_seq)
     plt.plot(distances)
-    peaks = find_peaks(distances)[0]
-    peak_vals = [distances[x] for x in peaks]
-    # print(peaks, peak_vals)
-    # quit()
     plt.scatter(peaks, peak_vals)
     plt.savefig("output.png")
-    # plt.show()
     plt.close()
     quit()
 
-show_gait_cycle(skel_da.norm_skeletons[2])
+show_gait_cycle(skel_da.norm_skeletons[0])
 
